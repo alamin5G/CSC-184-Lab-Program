6a6d74e20446589e2d1eee32e145bd62e9b1f86e
alamin5g <alaminrakib4864@gmail.com>
2022-03-11 13:28:32 +0600

    https://prnt.sc/6WUKwOyYb8Ha see the sample program

---
+++ problem-16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+    int row, column, n;
+    printf("Enter row number: ");
+    scanf("%d", &n);
+
+    for(row = 0; row<n; row++){
+        for(column=n; column>row; column--){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
