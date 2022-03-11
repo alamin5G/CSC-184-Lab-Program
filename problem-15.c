95482a6e8849015bbb81c93c1519372aac2437b4
alamin5g <alaminrakib4864@gmail.com>
2022-03-11 13:22:34 +0600

    https://prnt.sc/eBp0FU6r9HDt see the sample

---
+++ problem-15.c
@@ -0,0 +1,22 @@
+// a program for triangle
+#include <stdio.h>
+
+
+void main(){
+
+    int row, column, n;
+    
+    printf("Enter row : ");
+    scanf("%d",&n);
+
+    for ( row = 0; row < n; row++)
+    {
+        for(column = 0; column<=row; column++){
+            printf("%d ",column+1);
+        }
+        printf("\n");
+    }
+    
+    
+
+}
