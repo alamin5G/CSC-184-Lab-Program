b5e0db016a7bade949cdd483d8133e55ba3077d4
alamin5g <alaminrakib4864@gmail.com>
2022-03-11 14:30:27 +0600

    https://prnt.sc/fZ62KmyZlFnq see the output

---
+++ problem-20.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main(){
+    int row, column, n;
+    printf("Enter row number: ");
+    scanf("%d", &n);
+    for(row = 1; row<=n; row++){
+        for(column=n; column>=1; column--){
+            if(row < column){
+                printf(" ");
+            }else{
+                printf("*");
+            }
+        }
+        printf("\n");   
+    }
+}
