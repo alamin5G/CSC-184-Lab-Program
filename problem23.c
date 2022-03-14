0ecba138fd8d93fef892965669ddf53e8390436c
alamin5g <alaminrakib4864@gmail.com>
2022-03-15 05:18:51 +0600

    sort array elements in asscending order

--- problem23.c
+++ problem23.c
@@ -1,23 +1,13 @@
 /**
  * @file problem23.c
-<<<<<<< HEAD
- * sort array elements in descending order
+ * sort array elements in asscending order
  * @author Md. Alamin (alamin5g@yahoo.com)
  * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
  * @version 0.1
  * @date 2022-03-15
-=======
- * Sort array elements in ascending order
- * @author Md. Alamin (alamin5g@yahoo.com)
- * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
- * @version 0.1
- * @date 2022-03-14
->>>>>>> 4f1f8924d35cf907a2a3a3f2f5ab5b00a32b4c2c
- * 
  * @copyright Copyright (c) 2022
  * 
  */
-<<<<<<< HEAD
 #include <stdio.h>
  void main(){
      int a[10], p, q, n, temp;
@@ -32,7 +22,7 @@
      for (p=0; p <n; p++)
      {
          for(q=p+1; q<n; q++){
-             if (a[p]<a[q])
+             if (a[p]>a[q])
              {
                  temp = a[p];
                  a[p] = a[q];
@@ -46,33 +36,3 @@
      }
 
  }
-=======
-#include <stdio.h> 
-void main(){
-    int A[10], p, q, temp,n;
-    printf("How many array elements? ");
-    scanf("%d", &n);
-    printf("Enter %d array elements: ", n);
-    for(p=0; p<n; p++){
-        scanf("%d", &A[p]);
-    }
-
-    for(p=0; p<n; p++){
-        for (q = p+1; q<n; q++)
-        {
-            if (A[p]>A[q])
-            {
-                temp = A[p];
-                A[p] = A[q];
-                A[q] = temp;
-            }
-            
-        }
-        
-    }
-    printf("The elements are sorted in Asscending Order: ");
-    for(p=0; p<n; p++){
-        printf("%d\t", A[p]);
-    }
-}
->>>>>>> 4f1f8924d35cf907a2a3a3f2f5ab5b00a32b4c2c
